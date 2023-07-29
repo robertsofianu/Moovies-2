6571f655a4f2cbc032a58574f427e56904bdb03e
Robert Sofianu <robertsofianu@gmail.com>
2023-07-29 15:01:32 +0300

    inainte de schmabri

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
+import pathlib
+
+import google.auth.transport.requests
+import requests
 from app_fct_login import *
 from app_fct_main import *
-from flask import (Flask, abort, flash, jsonify, redirect, render_template, request,
-                   session, url_for)
+from flask import (Flask, abort, flash, jsonify, redirect, render_template,
+                   request, session, url_for)
+from google.oauth2 import id_token
 from google_auth_oauthlib import flow
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+
 
 app = Flask(__name__)
 app.secret_key=fct_hash_str()
@@ -10,6 +19,13 @@ app.secret_key=fct_hash_str()
 fct_creare_DB()
 GOOGLE_CLIENT_ID = '532617767739-j8vtdh7u78v2arb7od889s3nfu505bjo.apps.googleusercontent.com'
 
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, r"C:\Users\rober\OneDrive\Desktop\Programming\_movies_unboxing\json\google_client_id.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/dashboard"
+)
 
 
 def login_is_required(funtion):
@@ -27,7 +43,34 @@ def home():
     
     return render_template('index.html', choiced = posters)
 
+@app.route("/googlelogin")
+def googlelogin():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/dashboard")
 
 @app.route("/loginpage")
 def loginpage():
